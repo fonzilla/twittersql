1e92c2a1dfe8811c5904449096dd2283a4b862b1
fonzilla <j.alfonso.antonio@gmail.com>
2016-05-02 10:58:46 -0400

    trying to add hashtags

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-// var tweetBank = require('../tweetBank');
-// var pg = require('pg');
-// var conString = 'postgres://localhost:5432/twitterjs';
-// var client = new pg.Client(conString);
 
 module.exports = function makeRouterWithSockets (io, client) {
 
@@ -12,7 +8,7 @@ module.exports = function makeRouterWithSockets (io, client) {
   function respondWithAllTweets (req, res, next){
    client.query('SELECT * FROM tweets JOIN users ON tweets.userid = users.id', function (err, result) {
     var tweets = result.rows;
-    console.log(tweets);
+    //console.log(tweets);
     res.render('index', { title: 'Twitter.js', tweets: tweets });
     });
   }
@@ -26,7 +22,7 @@ module.exports = function makeRouterWithSockets (io, client) {
     var name = req.params.username;
     client.query('SELECT * FROM tweets JOIN users ON tweets.userid = users.id WHERE name=$1',[name], function (err, result) {
       var tweets = result.rows;
-      console.log(tweets);
+      //console.log(tweets);
         res.render('index', {
           title: 'Twitter.js',
           tweets: tweets,
@@ -42,7 +38,7 @@ module.exports = function makeRouterWithSockets (io, client) {
     var id = req.params.id;
     client.query('SELECT * FROM tweets JOIN users ON tweets.userid = users.id WHERE tweets.id=$1',[id], function (err, result) {
       var tweets = result.rows;
-      console.log(tweets);
+      //console.log(tweets);
       res.render('index', {
         title: 'Twitter.js',
         tweets: tweets // an array of only one element ;-)
@@ -54,25 +50,43 @@ module.exports = function makeRouterWithSockets (io, client) {
     var name = req.body.name;
     var text = req.body.text;
     var hashtags = text.match(/\S*#\S+/gi);
+    var tweet_id = client.query('SELECT id FROM tweets', function(err,result){
+      var arr = result.rows;
+      var lastID = arr.length+1;
+      return lastID;
+      //console.log(arr, lastID);
+    })
     var user_id = client.query('SELECT id FROM users WHERE name=$1',[name],function (err,result){
-      var id = result.rows[0].id;
-      client.query('INSERT INTO tweets (userId,content) VALUES ($1, $2)',[id, text],function(err,result){
-          if(hashtags.length){
-            hashtags.forEach(function(hash){
-              client.query('INSERT INTO ')
-            })
-          }
-
-          res.redirect('/');
-      });
+      //console.log(result);
+      return result.rows[0].id;
     });
+    
+    // console.log(tweet_id, 'HELLO HELLO HELLO');
+
+    client.query('INSERT INTO tweets (userId,content) VALUES ($1, $2)',[user_id, text],function(err,result){
+
+    });
+          
+    if(hashtags.length){
+      hashtags.forEach(function(hash){
+        client.query('INSERT INTO hashtags (tag) VALUES ($1)', [hash],function(err,result){
+        });
+        var tag_id = client.query('SELECT id FROM hashtags WHERE tag=$1', [hash], function(err,result){
+          return result.rows[0].id;
+        });
+        client.query('INSERT INTO tag_tweet (tagid,tweetid) VALUES ($1, $2)', [tag_id,tweet_id], function(err,result){
+        });
+      });
+    };
+
+    res.redirect('/');
 });
 
-CREATE TABLE tag_tweet (
-id SERIAL PRIMARY KEY,
-tagid INTEGER REFERENCES  hashtags(id) NOT NULL,
-tweetid INTEGER REFERENCES tweets(id) NOT NULL
-);
+// CREATE TABLE tag_tweet (
+// id SERIAL PRIMARY KEY,
+// tagid INTEGER REFERENCES  hashtags(id) NOT NULL,
+// tweetid INTEGER REFERENCES tweets(id) NOT NULL
+// );
 
   // // replaced this hard-coded route with general static routing in app.js
   // router.get('/stylesheets/style.css', function(req, res, next){
